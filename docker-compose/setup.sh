a3b1f930f865ecc0677546fad24d278c70f4c9c3
Frank Odongkara <fodongkara@gmail.com>
2025-04-22 18:01:54 +0300

    Update docker setup with non-interactive mode

--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
+docker-compose exec -T app composer install --no-interaction --prefer-dist --optimize-autoloader
 
-docker-compose exec app php artisan storage:link || true
-docker-compose exec app php artisan key:generate
+docker-compose exec -T app php artisan storage:link || true
+echo "yes" | docker-compose exec -T app php artisan key:generate
